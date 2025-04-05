b25cd83296d3aee44ec5e5a45b179a0f13ac6ea5
Anuj <Anuj201982@gmail.com>
2025-04-05 05:41:15 +0530

    retry

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ function NavBar() {
     }
 
     const handleSearchSubmit = (event) => {
-        event.preventDefault(); // Prevent page reload
+        event.preventDefault();
         setSearch(keyword);
         setCategory("");
     }

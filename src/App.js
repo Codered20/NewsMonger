17c95ecdec07969376680c1f225568faeb963e9d
Anuj <Anuj201982@gmail.com>
2025-04-05 05:49:09 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from './components/NavBar';
 import News from './components/News';
+import { useContext } from 'react';
+import { AppContext } from './components/context/context';
 
 function App() {
   console.log("News Application");
+  const [keyword] = useContext(AppContext)
   return (
     <div>
       <Router>
         <NavBar />
         <Routes>
-          <Route exact path="/" element={<News category="general" />} />
-          <Route exact path="/business" element={<News category="business" />} />
-          <Route exact path="/entertainment" element={<News category="entertainment" />} />
-          <Route exact path="/health" element={<News category="health" />} />
-          <Route exact path="/science" element={<News category="science" />} />
-          <Route exact path="/sports" element={<News category="sports" />} />
-          <Route exact path="/technology" element={<News category="technology" />} />
-          <Route exact path="/:keyword" element={<News />} />
+          <Route exact path="/NewsMonger" element={<News category="general" />} />
+          <Route exact path="/NewsMonger/business" element={<News category="business" />} />
+          <Route exact path="/NewsMonger/entertainment" element={<News category="entertainment" />} />
+          <Route exact path="/NewsMonger/health" element={<News category="health" />} />
+          <Route exact path="/NewsMonger/science" element={<News category="science" />} />
+          <Route exact path="/NewsMonger/sports" element={<News category="sports" />} />
+          <Route exact path="/NewsMongertechnology" element={<News category="technology" />} />
+          <Route exact path="/NewsMonger/keyword" element={<News />} />
         </Routes>
       </Router>
     </div>
